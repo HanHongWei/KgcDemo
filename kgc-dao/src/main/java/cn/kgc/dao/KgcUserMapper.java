6ffd18888c3fdff6a5016651906e9de21820ad00
hanhongwei <18813017704@163.com>
2018-08-11 11:23:03 +0800

    修改了部分文件（添加user , 添加pom.xml，在dao层中添加关系依赖）

--- kgc-dao/src/main/java/cn/kgc/dao/KgcUserMapper.java
+++ kgc-dao/src/main/java/cn/kgc/dao/KgcUserMapper.java
@@ -1,4 +1,11 @@
 package cn.kgc.dao;
 
+import cn.kgc.entity.User;
+
+import java.util.List;
+
 public interface KgcUserMapper {
+
+    List<User> queryAllUser();
+
 }
--- kgc-entity/src/main/java/cn/kgc/entity/User.java
+++ kgc-entity/src/main/java/cn/kgc/entity/User.java
@@ -6,4 +6,40 @@ package cn.kgc.entity;
  * @Author 韩宏伟
  **/
 public class User {
+    private Integer userId;//用户ID 注解
+    private String userName;//用户名 必填
+    private String userPassword;//用户名 必填
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userPassword='" + userPassword + '\'' +
+                '}';
+    }
 }
